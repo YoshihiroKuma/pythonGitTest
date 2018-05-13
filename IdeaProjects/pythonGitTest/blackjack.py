b9bb51ca76f17c1917d63d150e886d945ef75730
Yoshihiro Kuma <madogiwa429@gmail.com>
2018-05-13 22:37:07 +0900

    [modified]blackjack.pyにshuffle関数追加

--- IdeaProjects/pythonGitTest/blackjack.py
+++ IdeaProjects/pythonGitTest/blackjack.py
@@ -1,3 +1,14 @@
+import random
+
+
+def make_deck():
+    suits = ['S', 'H', 'D', 'C']  # 記号の定義
+    ranks = range(1, 14)          # 数字の定義
+    deck = [(x, y) for x in ranks for y in suits]
+    random.shuffle(deck)
+    return deck
+
+
 def main():
     turn = 1
     player_money = 100
